3a79dfa7b7ce4d4b3a3df5919d7d12b4512e77a1
Lucas Puerari <lucas.puerari@mia-care.io>
2022-10-29 13:27:28 +0200

    Fix lint warnings

--- src/app.py
+++ src/app.py
@@ -1,3 +1,7 @@
+"""
+Where everything begins...
+"""
+
 import uvicorn
 from fastapi import FastAPI
 
@@ -6,9 +10,11 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello World!"}
+def hello_world():
+    """Say Hello"""
+
+    return { "message": "Hello World!" }
 
 
 if __name__ == '__main__':
-  uvicorn.run(app, host="0.0.0.0", port=3000)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
--- src/test_sample.py
+++ src/test_sample.py
@@ -1,3 +1,7 @@
+"""
+This is a sample test file
+"""
+
 from fastapi.testclient import TestClient
 
 from app import app
@@ -5,7 +9,11 @@ from app import app
 
 client = TestClient(app)
 
-def test_read_main():
+
+def test_hello_world():
+    """Test say Hello"""
+
     response = client.get("/")
+
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World!"}
+    assert response.json() == { "message": "Hello World!" }
